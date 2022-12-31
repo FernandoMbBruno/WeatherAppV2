18b4c2a78a126c21687d77c95888d1f5812641d5
FernandoBruno <fernandomatheusbruno@hotmail.com>
2022-12-31 02:29:15 -0300

    Initial Commit

---
+++ WeatherAppV2/WeatherAppV2App.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppV2App.swift
+//  WeatherAppV2
+//
+//  Created by Fernando Matheus Ferreira Adomaitis Bruno on 31/12/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
